7a51d5f349b706125a12639a72e1eb2bc6cbe2a9
Daniel Röwenstrunk <roewenstrunk@edirom.de>
2018-12-03 10:35:31 +0100

    Affiliation Hadjakos

--- src/views/Program.vue
+++ src/views/Program.vue
@@ -49,7 +49,7 @@
                     <span>Musikethnologie</span><br/>
                     <span class="speaker">N.N.</span><br/>
                     <span>Music Information Retrieval</span><br/>
-                    <span class="speaker">Prof. Dr. Aristotelis Hadjakos</span><br/>
+                    <span class="speaker">Prof. Dr. Aristotelis Hadjakos, Zentrum Musik- und Filminformatik, Hochschule für Musik Detmold</span><br/>
                     <span>Editionsphilologie</span><br/>
                     <span class="speaker">Peter Stadler M.A., Carl-Maria-von-Weber-Gesamtausgabe, Universität Paderborn</span><br/>
                     <span>Musikbezogene Metadaten</span><br/>
